f4cf3aa5545322d7e8f1590beba5b724fc1093a4
tylefnx <tayfunucuncu@gmail.com>
2024-10-02 18:52:00 +0300

    Added quickstart text

--- lib/core/presentation/main_page.dart
+++ lib/core/presentation/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gymlogger/core/presentation/app_padding.dart';
+import 'package:gymlogger/core/presentation/app_text.dart';
 import 'package:gymlogger/core/presentation/bottom_navigation_bar/bottom_navigation_bar.dart';
 import 'package:gymlogger/core/presentation/bottom_navigation_bar/bottom_navigation_items.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -19,8 +21,12 @@ class MainPage extends HookConsumerWidget {
         bottomNavigationBar: CustomBottomNavigationBar(
           selectedIndex: bottomNavigationIndex,
         ),
-        body: SingleChildScrollView(
-          child: SizedBox(),
+        body: AppPadding.h10v20(
+          child: Column(
+            children: [
+              AppText(text: 'Quick Start'),
+            ],
+          ),
         ),
       ),
     );
