591c5d46284de5c87ac904c66f0a0afaf7f4e3da
tylefnx <tayfunucuncu@gmail.com>
2024-10-06 17:30:51 +0300

    added padding

--- lib/core/presentation/main_page.dart
+++ lib/core/presentation/main_page.dart
@@ -47,6 +47,27 @@ class MainPage extends HookConsumerWidget {
               SB_AppPadding.h10(),
               AppAddButton(
                 text: 'Start Empty Workout',
+              ),
+              SB_AppPadding.h10(),
+              AppText.big_bold(text: 'Routines'),
+              SB_AppPadding.h10(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: AppTallButton(
+                      text: 'New Routine',
+                      icondata: Icons.bookmark_add,
+                    ),
+                  ),
+                  SB_AppPadding.w15(),
+                  Expanded(
+                    child: AppTallButton(
+                      text: 'Explore',
+                      icondata: Icons.search,
+                    ),
+                  ),
+                ],
               )
             ],
           ),
