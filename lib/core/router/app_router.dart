08ea4ddfaf96cfeced818a7978263d03f0d74a41
tylefnx <tayfunucuncu@gmail.com>
2024-10-12 18:50:53 +0300

    Added 1rm calculator to routes

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:gymlogger/calculator/presentation/1rm_calculator.dart';
 import 'package:gymlogger/main_page/main_page.dart';
 
 class AppRoute {
@@ -10,6 +11,12 @@ class AppRoute {
         path: "/",
         builder: (context, state) {
           return const MainPage();
+        },
+      ),
+      GoRoute(
+        path: "/calculators/1RM",
+        builder: (context, state) {
+          return const OneRepMaxCalculator();
         },
       ),
     ],
