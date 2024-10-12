f0077a3c660318d843a1bf78636201a5b1c253ac
tylefnx <tayfunucuncu@gmail.com>
2024-10-12 18:51:04 +0300

    Created 1rm calculator screen

---
+++ lib/calculator/presentation/1rm_calculator.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OneRepMaxCalculator extends StatelessWidget {
+  const OneRepMaxCalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(),
+    );
+  }
+}
