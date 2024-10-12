4a5c5ca777a566d3c4fb2de3242ec8b6df15c446
tylefnx <tayfunucuncu@gmail.com>
2024-10-12 15:40:24 +0300

    Changed bottom navigation widget to hookwidget

--- lib/core/presentation/bottom_navigation_bar/bottom_navigation_bar.dart
+++ lib/core/presentation/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gymlogger/core/presentation/bottom_navigation_bar/bottom_navigation_items.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class CustomBottomNavigationBar extends HookConsumerWidget {
+class CustomBottomNavigationBar extends HookWidget {
   final ValueNotifier<int> selectedIndex;
   const CustomBottomNavigationBar({
     required this.selectedIndex,
     super.key,
   });
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: selectedIndex.value,
       fixedColor: Colors.blueAccent,
