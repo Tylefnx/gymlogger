123b4211fe8aa24d4ed2b609c8301fcd737d97bb
tylefnx <tayfunucuncu@gmail.com>
2024-10-16 18:16:15 +0300

    added important import

--- lib/calculator/presentation/1rm_calculator.dart
+++ lib/calculator/presentation/1rm_calculator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gymlogger/core/presentation/app_padding.dart';
 
 class OneRepMaxCalculator extends StatelessWidget {
   const OneRepMaxCalculator({super.key});
@@ -7,7 +8,26 @@ class OneRepMaxCalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(),
+      body: Column(
+        children: [
+          AppPadding.h10v20(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: 30, maxWidth: 65),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
