016c4df2076fa8460b065fa3bfb84073d57a500a
tylefnx <tayfunucuncu@gmail.com>
2024-10-20 02:03:31 +0300

    Added weight Apptextformfield

--- lib/calculator/presentation/strength_level.dart
+++ lib/calculator/presentation/strength_level.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gymlogger/core/presentation/app_padding.dart';
+import 'package:gymlogger/core/presentation/app_text_form_field.dart';
 
-class StrengthLevelCalculator extends StatelessWidget {
+class StrengthLevelCalculator extends HookWidget {
   const StrengthLevelCalculator({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final weightController = useTextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: Text('Strength Level Calculator'),
       ),
-      body: Column(
-        children: [
-          TextFormField(),
-          TextFormField(),
-          TextFormField(),
-        ],
+      body: AppPadding.h30v40(
+        child: Column(
+          children: [
+            AppTextFormField.numberWithOptions(
+              label: 'Weight',
+              controller: weightController,
+            ),
+          ],
+        ),
       ),
     );
   }
