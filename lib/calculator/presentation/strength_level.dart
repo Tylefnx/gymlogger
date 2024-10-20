b444857be3eef6f8dad33320dc593daf5c2e9c4d
tylefnx <tayfunucuncu@gmail.com>
2024-10-20 19:19:25 +0300

    added dropdownbutton to strengthlevel calculator

--- lib/calculator/presentation/strength_level.dart
+++ lib/calculator/presentation/strength_level.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gymlogger/core/presentation/app_dropdown_button.dart';
 import 'package:gymlogger/core/presentation/app_padding.dart';
 import 'package:gymlogger/core/presentation/app_text_form_field.dart';
 
@@ -19,6 +20,46 @@ class StrengthLevelCalculator extends HookWidget {
               label: 'Weight',
               controller: weightController,
             ),
+            AppDropDownButton(
+              items: [
+                DropdownMenuItem(
+                  child: Text(
+                    'Squat',
+                  ),
+                  value: 'Squat',
+                ),
+                DropdownMenuItem(
+                  child: Text(
+                    'Bench Press',
+                  ),
+                  value: 'Bench Press',
+                ),
+                DropdownMenuItem(
+                  child: Text(
+                    'Deadlift',
+                  ),
+                  value: 'Deadlift',
+                ),
+                DropdownMenuItem(
+                  child: Text(
+                    'Barbell Row',
+                  ),
+                  value: 'Barbell Row',
+                ),
+                DropdownMenuItem(
+                  child: Text(
+                    'Overhead Press',
+                  ),
+                  value: 'Overhead Press',
+                ),
+                DropdownMenuItem(
+                  child: Text(
+                    'Weighted Dips',
+                  ),
+                  value: 'Weighted Dips',
+                ),
+              ],
+            )
           ],
         ),
       ),
