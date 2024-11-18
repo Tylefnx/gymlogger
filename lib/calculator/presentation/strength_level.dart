84f72840921ecc3343089a38aee43c4127059017
tylefnx <tayfunucuncu@gmail.com>
2024-11-18 21:09:47 +0300

    -

--- lib/calculator/presentation/strength_level.dart
+++ lib/calculator/presentation/strength_level.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gymlogger/calculator/shared/dropdowns.dart';
 import 'package:gymlogger/core/constant_lists.dart';
 import 'package:gymlogger/core/presentation/app_buttons.dart';
 import 'package:gymlogger/core/presentation/app_dropdown_button.dart';
@@ -80,49 +81,6 @@ class GenderSelectionWidget extends StatelessWidget {
           child: AppText.bold(text: _ ?? ''),
         );
       }).toList(),
-    );
-  }
-}
-
-class ExcersizeSelectionSection extends StatelessWidget {
-  const ExcersizeSelectionSection({
-    super.key,
-    required this.excersize,
-    required this.strengthLevelExcersize,
-  });
-
-  final ValueNotifier<String?> excersize;
-  final List<String?> strengthLevelExcersize;
-
-  @override
-  Widget build(BuildContext context) {
-    return AppDropDownButton<String?>(
-      hint: 'Select Excersize',
-      value: excersize.value,
-      onChanged: (_) => excersize.value = _!,
-      items: strengthLevelExcersize.map<DropdownMenuItem<String?>>((String? _) {
-        return DropdownMenuItem(
-          value: _,
-          child: AppText.bold(text: _ ?? ''),
-        );
-      }).toList(),
-    );
-  }
-}
-
-class WeightSelectionSection extends StatelessWidget {
-  const WeightSelectionSection({
-    super.key,
-    required this.weightController,
-  });
-
-  final TextEditingController weightController;
-
-  @override
-  Widget build(BuildContext context) {
-    return AppTextFormField.numberWithOptions(
-      label: 'Weight',
-      controller: weightController,
     );
   }
 }
