541caa9b6faf321564a38e4d2c0a56334c4d6132
tylefnx <tayfunucuncu@gmail.com>
2024-11-18 20:15:07 +0300

    Created FatPercentage calculator page

---
+++ lib/calculator/presentation/fat_percentage_calculator.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FatPercentageCalculator extends StatelessWidget {
+  const FatPercentageCalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
