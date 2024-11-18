98527aae168e4730822bb83c40d372b07b6739f8
tylefnx <tayfunucuncu@gmail.com>
2024-11-18 21:03:22 +0300

    fat percentage calculator function written

--- lib/calculator/presentation/fat_percentage_calculator.dart
+++ lib/calculator/presentation/fat_percentage_calculator.dart
@@ -1,3 +1,4 @@
+import 'dart:math' as m;
 import 'package:flutter/material.dart';
 
 class FatPercentageCalculator extends StatelessWidget {
@@ -5,6 +6,26 @@ class FatPercentageCalculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
   }
+}
+
+double calculateBodyFat(
+  String gender,
+  double waist,
+  double neck,
+  double height,
+  double? hip,
+) {
+  if (gender == 'male') {
+    return 86.010 * m.log(waist - neck) - 70.041 * m.log(height) + 36.76;
+  } else if (gender == 'woman' && hip != null) {
+    return 163.205 * m.log(waist + hip - neck) -
+        97.684 * m.log(height) -
+        78.387;
+  }
+  return 0;
 }
