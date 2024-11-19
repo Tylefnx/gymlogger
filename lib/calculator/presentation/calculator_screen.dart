b2d55da96b4c45d7ec09c4eb68c0a0062788d986
tylefnx <tayfunucuncu@gmail.com>
2024-11-20 02:40:07 +0300

    sadsda

--- lib/calculator/presentation/calculator_screen.dart
+++ lib/calculator/presentation/calculator_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:gymlogger/core/presentation/app_gridview.dart';
 import 'package:gymlogger/core/presentation/app_text.dart';
 
 class CalculatorsScreen extends StatelessWidget {
@@ -7,65 +8,24 @@ class CalculatorsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisSpacing: 20,
-      mainAxisSpacing: 20,
-      padding: EdgeInsets.symmetric(
-        horizontal: 40,
-        vertical: 10,
-      ),
-      crossAxisCount: 2,
+    return AppGridviewWidget(
       children: [
-        CalculatorSectionCard(
+        GridviewSectionCard(
           label: '1RM',
           icon: Icons.fitness_center,
           onTap: () => GoRouter.of(context).push('/calculators/1RM'),
         ),
-        CalculatorSectionCard(
+        GridviewSectionCard(
           label: 'Strength Level',
           icon: Icons.fitness_center_outlined,
           onTap: () => GoRouter.of(context).push('/calculators/str_level'),
         ),
-        CalculatorSectionCard(
+        GridviewSectionCard(
           label: 'Fat Percentage',
           icon: Icons.generating_tokens_sharp,
           onTap: () => GoRouter.of(context).push('/calculators/fat_percentage'),
         ),
       ],
-    );
-  }
-}
-
-class CalculatorSectionCard extends StatelessWidget {
-  final void Function() onTap;
-  final String label;
-  final IconData icon;
-  const CalculatorSectionCard({
-    super.key,
-    required this.label,
-    required this.icon,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Card(
-        color: Colors.blueGrey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SizedBox(),
-            Icon(
-              icon,
-              size: 90,
-            ),
-            AppText.big_bold(text: label),
-          ],
-        ),
-      ),
     );
   }
 }
