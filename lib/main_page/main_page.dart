ac21e46f43ac1e635e5edec5613dea28f7f2d986
tylefnx <tayfunucuncu@gmail.com>
2024-11-20 05:14:22 +0300

    Flutter smart dialog initalized

--- lib/main_page/main_page.dart
+++ lib/main_page/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:gymlogger/core/presentation/app_text.dart';
 import 'package:gymlogger/core/presentation/bottom_navigation_bar/bottom_navigation_bar.dart';
 import 'package:gymlogger/core/presentation/bottom_navigation_bar/bottom_navigation_items.dart';
@@ -14,6 +15,7 @@ class MainScreen extends StatelessWidget {
     final _appRouter = AppRoute();
     return ProviderScope(
       child: MaterialApp.router(
+        builder: FlutterSmartDialog.init(),
         routerDelegate: _appRouter.router.routerDelegate,
         routeInformationParser: _appRouter.router.routeInformationParser,
         routeInformationProvider: _appRouter.router.routeInformationProvider,
