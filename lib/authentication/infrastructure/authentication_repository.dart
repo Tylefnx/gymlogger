8b9d6109755299ee574beafc54add07326da64ff
tylefnx <tayfunucuncu@gmail.com>
2024-12-03 04:42:21 +0300

    copy pasted authentication layer from another project

---
+++ lib/authentication/infrastructure/authentication_repository.dart
@@ -0,0 +1,48 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:gymlogger/authentication/domain/auth_failure.dart';
+import 'package:gymlogger/authentication/infrastructure/authentication_service.dart';
+
+class AuthenticationRepository {
+  final AuthenticationService _service;
+
+  const AuthenticationRepository(this._service);
+
+  Future<Either<AuthFailure, String>> login({
+    required String username,
+    required String password,
+  }) async {
+    try {
+      final response = await _service.login(
+        username: username,
+        password: password,
+      );
+      print(response);
+      final json = response.data as Map;
+      return Right(json['token'].toString());
+    } on DioException catch (e) {
+      return left(
+        AuthFailure.storage(e.message),
+      );
+    }
+  }
+
+  Future<Either<AuthFailure, String>> register({
+    required String username,
+    required String password,
+  }) async {
+    try {
+      final response = await _service.login(
+        username: username,
+        password: password,
+      );
+      final uid = response.data['user_id'] as String;
+
+      return Right(uid);
+    } on DioException catch (e) {
+      return left(
+        AuthFailure.storage(e.message),
+      );
+    }
+  }
+}
---
+++ lib/authentication/infrastructure/authentication_service.dart
@@ -0,0 +1,47 @@
+import 'package:dio/dio.dart';
+
+class AuthenticationService {
+  final Dio _dio;
+
+  const AuthenticationService(this._dio);
+  static const loginEndPoint = 'login';
+  static const registerEndPoint = 'register';
+  static const port = '8080';
+  static const base = 'http://localhost:$port';
+  Future<Response> login({
+    required String username,
+    required String password,
+  }) async {
+    final response = await _dio.post(
+      '$base/$loginEndPoint',
+      // '$base/$loginEndPoint',
+      data: {
+        'username': username,
+        'password': password,
+      },
+    );
+    return response;
+  }
+
+  Future<Response> register({
+    required String username,
+    required String password,
+  }) {
+    final response = _dio.post(
+      '$base/$registerEndPoint',
+      data: {
+        'username': username,
+        'password': password,
+      },
+    );
+    return response;
+  }
+
+  Future<Response> forgotPassword() {
+    final response = _dio.post(
+      '$base$loginEndPoint',
+      data: {},
+    );
+    return response;
+  }
+}
