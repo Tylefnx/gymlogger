775d55450591e28ea0980beb510eb17d1cdaea04
tylefnx <tayfunucuncu@gmail.com>
2024-12-04 10:05:30 +0300

    Fixed one more

--- lib/main_page/main_page.dart
+++ lib/main_page/main_page.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:gymlogger/authentication/application/authentication_notifier.dart';
 import 'package:gymlogger/authentication/presentation/login_screen.dart';
 import 'package:gymlogger/authentication/shared/providers.dart';
 import 'package:gymlogger/core/presentation/app_text.dart';
