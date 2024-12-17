fb5966cc62c06113ef6176a8793f4bea523666da
Tylefnx <tayfunucuncu@gmail.com>
2024-12-17 04:26:30 +0300

    Custom toasts added

---
+++ lib/core/shared/app_toasts.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:gymlogger/core/presentation/app_durations.dart';
+import 'package:gymlogger/core/presentation/app_text.dart';
+import 'package:toastification/toastification.dart';
+
+ToastificationItem failureToast({
+  required String error,
+  required BuildContext context,
+}) {
+  return toastification.show(
+    type: ToastificationType.error,
+    context: context,
+    autoCloseDuration: toastDuration,
+    title: AppText.bold(text: error),
+  );
+}
+
+ToastificationItem successToast({
+  required String success,
+  required BuildContext context,
+}) {
+  return toastification.show(
+    type: ToastificationType.success,
+    context: context,
+    autoCloseDuration: toastDuration,
+    title: AppText.bold(text: success),
+  );
+}
