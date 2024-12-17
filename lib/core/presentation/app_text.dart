4185a5966ac87688bc6b19a57535876b2ee67e8e
Tylefnx <tayfunucuncu@gmail.com>
2024-12-17 06:34:43 +0300

    added color parameter to AppText

--- lib/core/presentation/app_text.dart
+++ lib/core/presentation/app_text.dart
@@ -1,52 +1,66 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 class AppText extends StatelessWidget {
   final TextStyle? textStyle;
+  final Color? color;
   final String text;
-  const AppText({super.key, required this.text, this.textStyle});
+  const AppText({
+    super.key,
+    required this.text,
+    this.textStyle,
+    this.color,
+  });
 
-  factory AppText.normal({required String text}) {
+  factory AppText.normal({required String text, Color? color}) {
     return AppText(
       text: text,
-      textStyle: const TextStyle(),
-    );
-  }
-
-  factory AppText.bold({required String text}) {
-    return AppText(
-      text: text,
-      textStyle: const TextStyle(
-        fontWeight: FontWeight.bold,
+      textStyle: TextStyle(
+        color: color,
       ),
     );
   }
 
-  factory AppText.big_bold({required String text}) {
+  factory AppText.bold({required String text, Color? color}) {
     return AppText(
       text: text,
-      textStyle: const TextStyle(
+      textStyle: TextStyle(
+        fontWeight: FontWeight.bold,
+        color: color,
+      ),
+    );
+  }
+
+  factory AppText.big_bold({required String text, Color? color}) {
+    return AppText(
+      text: text,
+      textStyle: TextStyle(
         fontWeight: FontWeight.bold,
         fontSize: 16,
+        color: color,
       ),
     );
   }
 
-  factory AppText.little_big_bold({required String text}) {
+  factory AppText.little_big_bold({required String text, Color? color}) {
     return AppText(
       text: text,
-      textStyle: const TextStyle(
+      textStyle: TextStyle(
         fontWeight: FontWeight.bold,
         fontSize: 32,
+        color: color,
       ),
     );
   }
 
-  factory AppText.very_big_bold({required String text}) {
+  factory AppText.very_big_bold({required String text, Color? color}) {
     return AppText(
       text: text,
-      textStyle: const TextStyle(
+      textStyle: TextStyle(
         fontWeight: FontWeight.bold,
         fontSize: 48,
+        color: color,
       ),
     );
   }
