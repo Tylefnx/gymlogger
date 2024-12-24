5024ef59cb94ce237878e6c27e82ec5a842bf144
Tylefnx <tayfunucuncu@gmail.com>
2024-12-25 02:43:17 +0300

    new profile screen

--- lib/main_page/sub_screens/profile_screen.dart
+++ lib/main_page/sub_screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:gymlogger/core/presentation/app_padding.dart';
 import 'package:gymlogger/core/presentation/app_text.dart';
 import 'package:gymlogger/core/presentation/sb_app_padding.dart';
 
@@ -12,12 +13,34 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const ProfilePictureWidget(),
+            const Center(child: ProfilePictureWidget()),
             SB_AppPadding.h10(),
-            AppText.little_big_bold(text: 'EliteLifter'),
-            AppText.little_big_bold(text: 'abuzerkomurcu@gmail.com'),
-            AppText.little_big_bold(text: 'Abuzer Kömürcü'),
+            ListTile(
+              tileColor: Colors.grey[200],
+              leading: const Icon(Icons.person),
+              title: AppText.bold(text: 'Username:'),
+              subtitle: AppText.bold(text: 'EliteLifter'),
+            ),
+            ListTile(
+              tileColor: Colors.white,
+              leading: const Icon(Icons.mail),
+              title: AppText.bold(text: 'Email:'),
+              subtitle: AppText.bold(text: 'abuzerkomurcu@gmail.com'),
+            ),
+            ListTile(
+              tileColor: Colors.grey[200],
+              leading: const Icon(Icons.person_pin),
+              title: AppText.bold(text: 'Full Name:'),
+              subtitle: AppText.bold(text: 'Abuzer Kömürcü'),
+            ),
+            ListTile(
+              tileColor: Colors.white,
+              leading: const Icon(Icons.star),
+              title: AppText.bold(text: 'Overall Level:'),
+              subtitle: AppText.bold(text: 'Elite'),
+            ),
           ],
         ),
       ),
