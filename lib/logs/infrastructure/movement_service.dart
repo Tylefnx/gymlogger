ae1e45fb3a2ea08ae88b01c164c9e42db65c2802
Tylefnx <tayfunucuncu@gmail.com>
2025-01-14 02:39:44 +0300

    Created MovementLogService

---
+++ lib/logs/infrastructure/movement_service.dart
@@ -0,0 +1,75 @@
+import 'package:dio/dio.dart';
+
+class MovementLogsService {
+  final Dio _dio;
+
+  const MovementLogsService(this._dio);
+  static const saveEndpoint = '/save_user_lifts';
+  static const updateEndPoint = '/update_user_lifts';
+  static const deleteEndPoint = '/delete_user_lifts';
+  static const getEndPoint = '/get_all_user_lifts';
+
+  Future<Response> getUserLifts({
+    required String username,
+  }) async {
+    final response = await _dio.post(
+      getEndPoint,
+      data: {
+        'username': username,
+      },
+    );
+    return response;
+  }
+
+  Future<Response> saveUserLifts({
+    required String username,
+    required String exercize,
+    required String date,
+    required double weight,
+  }) async {
+    final response = await _dio.post(
+      saveEndpoint,
+      data: {
+        'username': username,
+        'exercize': exercize,
+        'date': date,
+        'weight': weight,
+      },
+    );
+    return response;
+  }
+
+  Future<Response> updateUserLifts({
+    required String username,
+    required String exercize,
+    required String date,
+    required double weight,
+  }) async {
+    final response = await _dio.post(
+      updateEndPoint,
+      data: {
+        'username': username,
+        'exercize': exercize,
+        'date': date,
+        'weight': weight,
+      },
+    );
+    return response;
+  }
+
+  Future<Response> deleteUserLifts({
+    required String username,
+    required String exercize,
+    required String date,
+  }) async {
+    final response = await _dio.post(
+      deleteEndPoint,
+      data: {
+        'username': username,
+        'exercize': exercize,
+        'date': date,
+      },
+    );
+    return response;
+  }
+}
