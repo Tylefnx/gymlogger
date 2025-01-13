daf88e4f805eb97a2b25824f8bd686a946adc02f
Tylefnx <tayfunucuncu@gmail.com>
2025-01-14 02:39:11 +0300

    commit above

--- lib/authentication/shared/providers.dart
+++ lib/authentication/shared/providers.dart
@@ -1,10 +1,8 @@
-import 'package:dio/dio.dart';
 import 'package:gymlogger/authentication/application/authentication_notifier.dart';
 import 'package:gymlogger/authentication/infrastructure/authentication_repository.dart';
 import 'package:gymlogger/authentication/infrastructure/authentication_service.dart';
+import 'package:gymlogger/core/shared/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final dio = Dio();
 
 final authServiceProvider = Provider(
   (ref) => AuthenticationService(dio),
