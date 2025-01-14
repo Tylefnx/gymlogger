b17b003e2e35d3ff6354e290ba54ca1dd2ac235a
Tylefnx <tayfunucuncu@gmail.com>
2025-01-14 07:03:30 +0300

    Created providers

---
+++ lib/logs/shared/providers.dart
@@ -0,0 +1,22 @@
+import 'package:gymlogger/core/shared/dio.dart';
+import 'package:gymlogger/logs/application/movements_notifier.dart';
+import 'package:gymlogger/logs/infrastructure/movement_repository.dart';
+import 'package:gymlogger/logs/infrastructure/movement_service.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final movementsServiceProvider = Provider(
+  (ref) => MovementLogsService(dio),
+);
+
+final movementsRepoProvider = Provider(
+  (ref) => MovementLogsRepository(
+    ref.watch(movementsServiceProvider),
+  ),
+);
+
+final movementsStateNotifierProvider =
+    StateNotifierProvider<MovementsStateNotifier, MovementsState>(
+  (ref) => MovementsStateNotifier(
+    ref.watch(movementsRepoProvider),
+  ),
+);
